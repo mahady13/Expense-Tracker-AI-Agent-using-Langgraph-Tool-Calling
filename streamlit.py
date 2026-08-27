7609a1d0c037dc34e089c5e693907187b195a220
mahady13 <mohiuddinmahady@gmail.com>
2026-08-27 08:41:46 +0600

    initial push

---
+++ streamlit.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import requests
+import uuid
+
+st.title("Expense Tracker AI Assistant")
+
+
+API_URL="http://127.0.0.1:8000/chat"
+
+if "thread_id" not in st.session_state:
+    st.session_state["thread_id"]=str(uuid.uuid4())
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[]
+
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+user_input=st.chat_input("What would you like to do sir?")
+
+if user_input:
+    st.session_state.messages.append({
+        "role":"user",
+        "content":user_input
+    })
+
+    response = requests.post(
+        API_URL,
+        json={
+            "thread_id": st.session_state.thread_id,
+            "message": user_input,
+            "user_id": "user123"
+        }
+    )
+
+    print("STATUS:", response.status_code)
+    print("RESPONSE:", response.text)
+
+    if response.status_code != 200:
+        st.error(f"Backend error {response.status_code}: {response.text}")
+        st.stop()
+
+    data = response.json()
+
+    data=response.json()
+
+    st.session_state.messages.append({
+        "role":"assistant",
+        "content":data["message"]
+    })
+
+    st.rerun()
